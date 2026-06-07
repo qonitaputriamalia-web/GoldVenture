d0c1c44df5a9d584a90a82996473c8138aaf4a76
Qonita putri amalia firdausah <qonitaputriamaliaa@gmail.com>
2026-06-07 16:28:51 +0700

    setup hero-section

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,10 +1,13 @@
 // import { useState } from 'react'
 import Header from "./components/header";
+import Hero from "./components/hero_section";
 
 function App() {
   return (
     <div>
       <Header />
+      <Hero />
+
     </div>
   )
 }
